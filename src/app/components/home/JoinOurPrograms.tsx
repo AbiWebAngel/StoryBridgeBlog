1c83bd92315f90626dd08e3308b654d6a6d18485
Prince <princesithole49@gmail.com>
2025-10-13 10:02:11 +0200

    Join Our Programs component

---
+++ src/app/components/home/JoinOurPrograms.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const programs = [
+  {
+    name: "Mentorship Program",
+    svg: "/assets/icons/home/mentorship.svg",
+    href: "/mentorship",
+  },
+  {
+    name: "Beta-Reading Program",
+    svg: "/assets/icons/home/betareading.svg",
+    href: "/beta-reading",
+  },
+  {
+    name: "Workshops",
+    svg: "/assets/icons/home/workshops.svg",
+    href: "/workshops",
+  },
+];
+
+export default function JoinOurPrograms() {
+  return (
+    <div className="w-full flex flex-col items-center gap-8 py-8 bg-transparent">
+      <div className="flex flex-wrap justify-center gap-24">
+        {programs.map((program) => (
+          <Link
+            key={program.name}
+            href={program.href}
+            className="flex flex-col items-center w-[250px] h-[250px] bg-[#EDE5D8] rounded-[30px] p-2 hover:shadow-lg transition"
+          >
+            <div className="w-[223px] h-[221px] relative">
+              <Image
+                src={program.svg}
+                alt={program.name}
+                fill
+                className="object-contain"
+              />
+            </div>
+            <h3 className="mt-2 font-cinzel font-bold text-[30px] text-center break-words">
+              {program.name}
+            </h3>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import SearchComponent from "./components/home/SearchComponent";
 import NewsletterForm from "./components/NewsletterForm";
 import SectionHeading from "./components/SectionHeading";
 import LatestBlogs from "./components/home/LatestBlogs";
+import JoinOurPrograms from "./components/home/JoinOurPrograms";
 
 export default function HomePage() {
 return (
@@ -15,7 +16,7 @@ return (
       {/* Latest Blogs Section */}
       <div className="mb-8">
           <SectionHeading
-            src="/assets/headings/home/Blog-posts.png"
+            src="/assets/headings/home/BlogPosts.png"
             alt="Blog Posts Heading"
             width={500}
             height={50}
@@ -24,6 +25,17 @@ return (
       </div>
       <LatestBlogs />
         
+      {/* Join Our Programs */}
+      <div className="mb-2 mt-16">
+          <SectionHeading
+            src="/assets/headings/home/JoinOurPrograms.png"
+            alt="Join Our Programs Heading"
+            width={300}
+            height={50}
+            center={true}
+          />
+      </div>
+      <JoinOurPrograms />
 
       {/* Newsletter */}
       <NewsletterForm />
