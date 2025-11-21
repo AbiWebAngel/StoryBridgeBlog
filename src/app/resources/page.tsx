5a8f5f1213cd1b76ef70337174a85a99d8d6f276
Prince <princesithole49@gmail.com>
2025-11-21 17:25:23 +0200

    Add resources page

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,14 @@
+import SectionHeading from "@/components/SectionHeading";
+import TextSection from "@/components/TextSection";
+
+
+export default function MentorshipPage() {
+
+  return  <main>
+  
+            {/* Resources page */}
+            <div className="mb-12">
+              <h1>Resources Page Here</h1>
+            </div>
+        </main>
+}
