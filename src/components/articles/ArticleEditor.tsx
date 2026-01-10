bf17f06766d5b00e98834f7e9ea13a91f6d4fe70
Prince <princesithole49@gmail.com>
2026-01-10 12:24:03 +0200

    Vercel fix

--- src/components/articles/ArticleEditor.tsx
+++ src/components/articles/ArticleEditor.tsx
@@ -26,6 +26,7 @@ import CharacterCount from '@tiptap/extension-character-count'
 
 
 
+
 interface ArticleEditorProps {
   value: any;
   articleId: string;
@@ -93,7 +94,7 @@ const applyYouTube = () => {
 
   
   console.log("YouTube node:", editor.getJSON());
-  
+
   // Close modal, clear input, and refocus
   setYoutubeModalOpen(false);
   setYoutubeUrl('');
@@ -446,6 +447,7 @@ const isHeading = editor?.isActive('heading')
             <path d="M384.85-220q-87.77 0-150.35-58.77t-62.58-144.69q0-85.92 62.58-144.5t150.35-58.58h280.84l-111.3-111.31L596.54-780 780-596.54 596.54-413.08l-42.15-42.15 111.3-111.31H384.85q-62.62 0-107.77 41.15-45.16 41.16-45.16 101.93 0 60.77 45.16 102.11Q322.23-280 384.85-280h287.07v60H384.85Z"/>
           </svg>
         </button>
+        
 
       </div>
 
